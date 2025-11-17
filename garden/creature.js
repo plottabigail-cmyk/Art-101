c67905906593d0b3ef57eb2402388323894d3cf5
aplott <plottabigail@gmail.com>
2025-11-16 20:17:35 -0800

    labs 2 and 3

--- garden/creature.js
+++ garden/creature.js
@@ -1,6 +1,6 @@
 
 //when the button is clicked (event)
-$("add-creature").click(function(){
+$("add-creature-twice").click(function(){
     // grab the value from the input
 
     //reachfor this input field and grab the text from it
@@ -11,3 +11,24 @@ $("add-creature").click(function(){
     $("crName").val("done!");
 })
 
+$("#add-creature").click(
+    function () {
+
+        // grab the value from the input
+        let crName = $("#crName").val();
+        let crColor= $("#crColor").val();
+        let crEyesNum= $("#crEyesNum").val();
+
+
+        // check for the field value do not add empty ones
+        if ( (crName == "") || (crName.length>12) ) { // do nothing 
+        }
+        else {
+            $("#creature-list").append("<div>" +crName + ", " +crColor + ", " +crEyesNum + "</div> ");
+        }
+
+        // remove the name after it's added
+        $("#crName").val("");
+
+    
+    });
